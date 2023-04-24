616156b770c2c30696b2c9e579069ba9f67269e4
Toni Massenberg <amassenberg@web.de>
2023-04-24 13:19:15 +0200

    basic map: if checkbox checked there's a dot

--- assets/map.js
+++ assets/map.js
@@ -6,11 +6,38 @@ oldmap.src = "images/basicmap.jpg"; //"<img src="images/basicmap.jpg" alt="Itali
 
 // handle checkboxes: https://www.javascripttutorial.net/javascript-dom/javascript-checkbox/
 const plan6 = document.querySelector('#plan6');
-console.log(plan6.checked);
+const plan7 = document.querySelector('#plan7');
 
-//function oldmap() {
-//    document.getElementById("oldmap").innerHTML = "Hello JavaScript function";
-//  }
+if (plan6.checked){
+        var container = document.querySelector('.container');
+
+        // create the icon element
+        var icon = document.createElement('i');
+        icon.classList.add('fa', 'fa-circle');
+        //icon.className = 'icon';
+        icon.style.position = 'absolute';
+        icon.style.width = '10px';
+        icon.style.height = '10px';
+
+        // append the icon element to the container element
+        container.appendChild(icon);
+
+        // calculate the percentage position of the icon
+        var containerWidth = container.offsetWidth;
+        var containerHeight = container.offsetHeight;
+        var iconTop = 40;
+        var iconLeft = 50;
+
+        icon.style.top = iconTop + '%';
+        icon.style.left = iconLeft + '%';
+}
+
+if (plan7.checked){
+    console.log("hello");
+}
+
+
+
 
 // Code for the Leaflat map of modern Florence. Reference: Agafonkin, Volodymyr. „Quick Start Guide - Leaflet - a JavaScript library for interactive maps“. Zugegriffen 22. April 2023. https://leafletjs.com/examples/quick-start/.
 var modernmap = L.map('modernmap').setView([43.7703,11.2574], 14);
