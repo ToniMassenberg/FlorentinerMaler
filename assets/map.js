e80cd8460475dd1c4592759fd3e63650041d88d7
Toni Massenberg <amassenberg@web.de>
2023-05-02 13:10:41 +0200

    get icon position from json, add and remove icon

--- assets/map.js
+++ assets/map.js
@@ -1,49 +1,76 @@
 // This file contains all code for the map.html subpage.
-const oldmap = document.getElementById("oldmap")
 
-// insert image in html
-oldmap.src = "images/basicmap.jpg"; //"<img src="images/basicmap.jpg" alt="Italian Trulli"></img>";
+// jQuery wrapper to only load JavaScript when HTML is fully loaded
+$(document).ready(() => {
 
-// handle checkboxes: https://www.javascripttutorial.net/javascript-dom/javascript-checkbox/
-const plan6 = document.querySelector('#plan6');
-const plan7 = document.querySelector('#plan7');
+  // insert image in html ("prepend" puts the entity first in the container)
+  $('#oldmap-container').prepend($('<img>', { id: 'oldmap', src: 'images/basicmap.jpg' }))
 
-if (plan6.checked){
-        var container = document.querySelector('.container');
 
-        // create the icon element
-        var icon = document.createElement('i');
-        icon.classList.add('fa', 'fa-circle');
-        //icon.className = 'icon';
-        icon.style.position = 'absolute';
-        icon.style.width = '10px';
-        icon.style.height = '10px';
+  // as soon as I use another file i get an error in the browser. to solve: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server
+  // to run this page in local server right-click HTML and select Launch in Browser (intro to extension: https://marketplace.visualstudio.com/items?itemName=yuichinukiyama.vscode-preview-server&ssr=false#overview)
 
-        // append the icon element to the container element
-        container.appendChild(icon);
 
-        // calculate the percentage position of the icon
-        var containerWidth = container.offsetWidth;
-        var containerHeight = container.offsetHeight;
-        var iconTop = 40;
-        var iconLeft = 50;
+  // checkbox with jQuery https://www.w3docs.com/snippets/javascript/how-to-test-if-a-checkbox-is-checked-with-jquery.html
+  $('input[id="plan6"]').click(function () {
+    // if plan6 is selected: 
+    if ($(this).prop("checked") == true) {
+      // get data for icon position from JSON
+      $.getJSON("assets/examplejson.json", function (data) {
+        var selectedPlan = "plan6";
+        // Get the painters for the selected plan
+        var painters = data[selectedPlan];
+        // Iterate over each painter
+        $.each(painters, function (painter, painterData) {
+          // Access the painter's data
+          var label = painterData.label;
+          var iconTop = painterData.iconTop;
+          var iconLeft = painterData.iconLeft;
+          console.log(painter, label, iconTop, iconLeft)
 
-        icon.style.top = iconTop + '%';
-        icon.style.left = iconLeft + '%';
-}
+          // create icon based on data
+          var container = document.querySelector('.container');
+          // create the icon element
+          var icon = document.createElement('i');
+          icon.classList.add('fa', 'fa-circle', 'plan6icon');
+          //icon.className = 'icon';
+          icon.style.position = 'absolute';
+          icon.style.width = '10px';
+          icon.style.height = '10px';
+          // append the icon element to the container element
+          container.appendChild(icon);
+          // place position as percentage
+          icon.style.top = iconTop + '%';
+          icon.style.left = iconLeft + '%';
+          console.log(icon)
+        
+        });
+      });
 
-if (plan7.checked){
+      // when the checkbox is unchecked again:  
+    } else if ($(this).prop("checked") == false) {
+      $('.plan6icon').remove();
+    }
+  });
+
+
+
+  $('#plan7').on('click', () => {
     console.log("hello");
-}
+  });
 
 
 
 
-// Code for the Leaflat map of modern Florence. Reference: Agafonkin, Volodymyr. „Quick Start Guide - Leaflet - a JavaScript library for interactive maps“. Zugegriffen 22. April 2023. https://leafletjs.com/examples/quick-start/.
-var modernmap = L.map('modernmap').setView([43.7703,11.2574], 14);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+
+  // Code for the Leaflat map of modern Florence. Reference: Agafonkin, Volodymyr. „Quick Start Guide - Leaflet - a JavaScript library for interactive maps“. Zugegriffen 22. April 2023. https://leafletjs.com/examples/quick-start/.
+  var modernmap = L.map('modernmap').setView([43.7703, 11.2574], 14);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(modernmap);
+  }).addTo(modernmap);
 
 
+
+})
