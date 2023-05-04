a16754986012ee93325604a042200212f5322a84
Toni Massenberg <amassenberg@web.de>
2023-05-04 09:34:30 +0200

    current state

--- assets/map.js
+++ assets/map.js
@@ -7,61 +7,59 @@ $(document).ready(() => {
   $('#oldmap-container').prepend($('<img>', { id: 'oldmap', src: 'images/basicmap.jpg' }))
 
 
-  // as soon as I use another file i get an error in the browser. to solve: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server
-  // to run this page in local server right-click HTML and select Launch in Browser (intro to extension: https://marketplace.visualstudio.com/items?itemName=yuichinukiyama.vscode-preview-server&ssr=false#overview)
+  // as soon as I use another file i get an error in the browser. To solve I set up a local testing server: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server
+  // to run this page in local server right-click HTML and select Launch in Browser (intro to extension in VSCode: https://marketplace.visualstudio.com/items?itemName=yuichinukiyama.vscode-preview-server&ssr=false#overview)
+
+  // function to add annotation regions based on coordinates in map-annotation-gonfaloni.json
 
 
-  // checkbox with jQuery https://www.w3docs.com/snippets/javascript/how-to-test-if-a-checkbox-is-checked-with-jquery.html
-  $('input[id="plan6"]').click(function () {
-    // if plan6 is selected: 
-    if ($(this).prop("checked") == true) {
-      // get data for icon position from JSON
-      $.getJSON("assets/examplejson.json", function (data) {
-        var selectedPlan = "plan6";
-        // Get the painters for the selected plan
-        var painters = data[selectedPlan];
-        // Iterate over each painter
-        $.each(painters, function (painter, painterData) {
-          // Access the painter's data
-          var label = painterData.label;
-          var iconTop = painterData.iconTop;
-          var iconLeft = painterData.iconLeft;
-          console.log(painter, label, iconTop, iconLeft)
 
-          // create icon based on data
-          var container = document.querySelector('.container');
-          // create the icon element
-          var icon = document.createElement('i');
-          icon.classList.add('fa', 'fa-circle', 'plan6icon');
-          //icon.className = 'icon';
-          icon.style.position = 'absolute';
-          icon.style.width = '10px';
-          icon.style.height = '10px';
-          // append the icon element to the container element
-          container.appendChild(icon);
-          // place position as percentage
-          icon.style.top = iconTop + '%';
-          icon.style.left = iconLeft + '%';
-          console.log(icon)
-        
+  // function to add icons to the map image based on coordinates in a JSON file. For checkbox use with jQuery: https://www.w3docs.com/snippets/javascript/how-to-test-if-a-checkbox-is-checked-with-jquery.html
+  function mapIcons(planNr, jsonFilePath) {
+    $('input[id="' + planNr + '"]').click(function () {
+      // if the checkbox is checked: 
+      if ($(this).prop("checked") == true) {
+        // get data for icon position from JSON
+        $.getJSON(jsonFilePath, function (data) {
+          var selectedPlan = planNr;
+          // Get the painters for the selected plan
+          var painters = data[selectedPlan];
+          // Iterate over each painter
+          $.each(painters, function (painter, painterData) {
+            // Access the painter's data
+            var label = painterData.label;
+            var iconTop = painterData.iconTop;
+            var iconLeft = painterData.iconLeft;
+  
+            // create icon based on data
+            var container = document.querySelector('.container');
+            // create the icon element
+            var icon = document.createElement('i');
+            icon.classList.add('fa', 'fa-circle', planNr + 'icon');
+            icon.style.position = 'absolute';
+            icon.style.width = '10px';
+            icon.style.height = '10px';
+            // append the icon element to the container element
+            container.appendChild(icon);
+            // place position as percentage
+            icon.style.top = iconTop + '%';
+            icon.style.left = iconLeft + '%';
+            console.log(icon)
+          });
         });
-      });
-
-      // when the checkbox is unchecked again:  
-    } else if ($(this).prop("checked") == false) {
-      $('.plan6icon').remove();
-    }
-  });
-
-
-
-  $('#plan7').on('click', () => {
-    console.log("hello");
-  });
-
-
-
-
+  
+        // when the checkbox is unchecked again:  
+      } else if ($(this).prop("checked") == false) {
+        $('.' + planNr + 'icon').remove();
+      }
+    });
+  }
+  
+  // call the function for each checkbox 
+  mapIcons("plan6", "assets/examplejson.json");
+  mapIcons("plan7", "assets/examplejson.json");
+  
+// place text: https://www.w3schools.com/howto/howto_css_image_text.asp
 
 
   // Code for the Leaflat map of modern Florence. Reference: Agafonkin, Volodymyr. „Quick Start Guide - Leaflet - a JavaScript library for interactive maps“. Zugegriffen 22. April 2023. https://leafletjs.com/examples/quick-start/.
@@ -74,3 +72,5 @@ $(document).ready(() => {
 
 
 })
+
+
