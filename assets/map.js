1059f4c6eded324d2cbf560a1ed2c78dfebc1510
Toni Massenberg <amassenberg@web.de>
2023-05-06 16:25:54 +0200

    coords for svg read from annotations json

--- assets/map.js
+++ assets/map.js
@@ -3,11 +3,45 @@
 // jQuery wrapper to only load JavaScript when HTML is fully loaded
 $(document).ready(() => {
 
-  
-  
+    // global variable with annotations for gonfaloni
+    const regionObjects = [];
+        // Load the JSON file with annotations
+        $.getJSON('assets/map-annotation-gonfaloni.json', function (data) {
+          // Create an array to hold the region objects
+          
+
+          // Loop through each region in the JSON file
+          for (const region of data.regions) {
+            const allPointsX = region.shape_attributes.all_points_x;
+            const allPointsY = region.shape_attributes.all_points_y;
+
+            // Combine the all_points_x and all_points_y arrays into a single string. This line was written by ChatGPT.
+            const pointsString = allPointsX.map((x, i) => `${x},${allPointsY[i]}`).join(' ');
+
+            // Create an object for the region with its name and formatted points string
+            const regionObj = {
+              name: region.region_attributes.name,
+              type: region.region_attributes.type,
+              avgWealth: region.region_attributes.avgwealth,
+              points: pointsString
+            };
+
+            // Add the region object to the array
+            regionObjects.push(regionObj);
+            
+          }
+
+          // Log the region objects to the console
+          //console.log('regionObjects: ' + JSON.stringify(regionObjects))
+          //return regionObjects
+        });
+
+
+
+
+
   // insert image in html ("prepend" puts the entity first in the container)
   //$('#oldmap-container').prepend($('<img>', { id: 'oldmap', src: 'images/basicmap.jpg' }))
-
 
 
   // as soon as I use another file i get an error in the browser. To solve I set up a local testing server: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server
@@ -19,22 +53,51 @@ $(document).ready(() => {
       // if the checkbox is checked: 
       if ($(this).prop("checked") == true) {
         // get data for annotations from JSON
-        //$.getJSON(jsonFilePath, function (data) {};
-        // jQuery can't be used directly on SVG elements because of different name spaces, discussion here: https://stackoverflow.com/questions/3642035/jquerys-append-not-working-with-svg-element - method here is the one suggested by forresto
-        $('#oldmap-svg-container').append($('<svg><polygon points="200,10 250,190 160,210" style="fill:lime;opacity:0.5" /></svg>'))
-        $('#oldmap-svg-container').append($('<svg><polygon points="200,100 250,200 160,220" style="fill:none;stroke:black;stroke-width:2" /></svg>'))
+
+
+        //console.log('regionObjects: ' + JSON.stringify(regionObjects))
+        
+        regionObjects.forEach(obj => {
+          const nameString = obj.name;
+          const pointsString = obj.points;
+          // The viewbox defines the SVG's size as the image size, preserveAspectRatio="none" means that the SVG overlay can be scaled and squished like the raster image instead of being cut off. Explanation for the use of SVGs: https://css-tricks.com/scale-svg/
+          $('#oldmap-container').append($('<div class="svg-container" id="generated"><svg viewBox="0 0 2178 2121" preserveAspectRatio="none"><polygon points="' + pointsString + '"style="fill:none;stroke:black;stroke-width:5" /><text x="200" y="100" fill="black">' + nameString + '</text></svg></div>'))
+
+
+        });
         
 
-    
 
-      // when the checkbox is unchecked again:  
+        // when the checkbox is unchecked again:  
       } else if ($(this).prop("checked") == false) {
-        $('#oldmap-svg-container').empty();
+        $('.svg-container').remove();
       }
     });
   }
-  
+
   mapAnnotations("plan2", "assets/map-annotation-gonfaloni.json")
+
+  function mapChoropleth(planNr, jsonFilePath) {
+    $('input[id="' + planNr + '"]').click(function () {
+      // if the checkbox is checked: 
+      if ($(this).prop("checked") == true) {
+        // get data for annotations from JSON
+        //$.getJSON(jsonFilePath, function (data) {};
+
+        var points = "'709,341 765,551 819,823 901,894 873,927 876,943 851,966 919,987 909,1035'"
+
+        // fill polygon for avg wealth map
+        $('#oldmap-svg-choropleth').append($('<svg viewBox="0 0 2178 2121" preserveAspectRatio="none"><polygon points=' + points + 'style="fill:lime;opacity:0.5" /></svg>'))
+
+
+        // when the checkbox is unchecked again:  
+      } else if ($(this).prop("checked") == false) {
+        $('#oldmap-svg-choropleth').empty();
+      }
+    });
+  }
+
+  mapChoropleth("choropleth", "assets/map-annotation-gonfaloni.json")
 
 
 
@@ -56,7 +119,7 @@ $(document).ready(() => {
             var iconLeft = painterData.iconLeft;
 
             // create icon based on data
-            var container = document.querySelector('.container');
+            var container = document.querySelector('#oldmap-container');
             // create the icon element
             var icon = document.createElement('i');
             icon.classList.add('fa', 'fa-circle', planNr + 'icon');
