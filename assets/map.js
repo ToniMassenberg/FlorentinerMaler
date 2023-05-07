95ac0e65a1755f0f1bfad3ee8851a77e61efc12c
Toni Massenberg <amassenberg@web.de>
2023-05-07 10:36:28 +0200

    annotations with text

--- assets/map.js
+++ assets/map.js
@@ -3,20 +3,28 @@
 // jQuery wrapper to only load JavaScript when HTML is fully loaded
 $(document).ready(() => {
 
-  // global object with annotations for gonfaloni so it can be accessed later
+  // Global array with the Objects holding gonfaloni info so it can be accessed later
   const regionObjects = [];
-  // how to log objects so you can see the content:
+  // How to log objects so you can see the content:
   // console.log('regionObjects: ' + JSON.stringify(regionObjects)) 
 
-  // get data for annotations from JSON here so it runs just once, not every time a checkbox is checked. The code to access the JSON file and reformat the pointsString was partly written by ChatGPT.
+  // Get data for annotations from JSON here so it runs just once, not every time a checkbox is checked. The code to access the JSON file and reformat the pointsString was partly written by ChatGPT and edited by me.
   $.getJSON('assets/map-annotation-gonfaloni.json', function (data) {
-    // Create an array to hold the region objects
 
 
     // Loop through each region in the JSON file
     for (const region of data.regions) {
       const allPointsX = region.shape_attributes.all_points_x;
       const allPointsY = region.shape_attributes.all_points_y;
+
+      // Calculate the middle point of a region so we can put descriptive text there.
+      let avgX = 0, avgY = 0, length = allPointsX.length;
+      for (let i = 0; i < length; i++) {
+        avgX += allPointsX[i];
+        avgY += allPointsY[i];
+      }
+      var middlePoint = `x="${avgX / length}" y="${avgY / length}"`;
+      // XX figure out better placement some other time
 
       // Combine the all_points_x and all_points_y arrays into a single string. 
       const pointsString = allPointsX.map((x, i) => `${x},${allPointsY[i]}`).join(' ');
@@ -26,7 +34,8 @@ $(document).ready(() => {
         name: region.region_attributes.name,
         type: region.region_attributes.type,
         avgWealth: region.region_attributes.avgwealth,
-        points: pointsString
+        points: pointsString,
+        middle: middlePoint
       };
       console.log(regionObj.type)
 
@@ -53,21 +62,29 @@ $(document).ready(() => {
         regionObjects.forEach(obj => {
           const nameString = obj.name;
           const pointsString = obj.points;
+          const middle = obj.middle;
 
+          // Define the style of the line.
           if (obj.type === "Quartieri") {
-            // Define the style of the line.
-            var styleString = "fill:none;stroke:black;stroke-width:4"
-          }
-
-          if (obj.type === "Gonfaloni") {
-            // Different line style for Gonfaloni: stroke-dasharray means dashed line
+            var styleString = "fill:none;stroke:black;stroke-width:5"
+            var textSize = "5em"
+          } else {
+            // Different line style for Gonfaloni: thinner, dashed line (stroke-dasharray)
             var styleString = "fill:none;stroke:black;stroke-dasharray:5,10;stroke-width:4"
+            var textSize = "3em"
           }
           // Insert SVG overlay as its own container since HTML can't handle SVG containers themselves, since they use different namespaces.
           // The viewbox defines the SVG's size as the image size, preserveAspectRatio="none" means that the SVG overlay can be scaled and squished like the raster image instead of being cut off. 
           // Explanation for the use of SVGs: https://css-tricks.com/scale-svg/
-          // Every city Quarter has its own overlay which means they properties can be changed individually. They have different name tags; if needed they could have eg. different colors or line patterns.
-          $('#oldmap-container').append($('<div class="svg-container" id="generated"><svg viewBox="0 0 2178 2121" preserveAspectRatio="none"><polygon points=" ' + pointsString + ' "style=' + styleString + ' /><text x="200" y="100" fill="black">' + nameString + '</text></svg></div>'))
+          // Every city quarter has its own overlay which means they properties can be changed individually. They have different name tags; if needed they could have eg. different colors or line patterns.
+          $('#oldmap-container').append(`
+            <div class="svg-container" id="generated">
+              <svg viewBox="0 0 2178 2121" preserveAspectRatio="none">
+                <polygon points="${pointsString}" style="${styleString}" />
+                <text font-size="${textSize}" ${middle} fill="black">${nameString}</text>
+              </svg>
+            </div>
+          `);
         });
 
         // when the checkbox is unchecked again remove overlays
@@ -146,7 +163,7 @@ $(document).ready(() => {
   mapAnnotations("plan2", "assets/map-annotation-gonfaloni.json")
   mapIcons("plan6", "assets/examplejson.json");
   mapIcons("plan7", "assets/examplejson.json");
-  
+
 
   // place text: https://www.w3schools.com/howto/howto_css_image_text.asp
 
