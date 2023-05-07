8ccc86da8aa54175ea74244cec0afe28d94afc93
Toni Massenberg <amassenberg@web.de>
2023-05-07 09:08:44 +0200

    cleaned up annotation code

--- assets/map.js
+++ assets/map.js
@@ -3,79 +3,80 @@
 // jQuery wrapper to only load JavaScript when HTML is fully loaded
 $(document).ready(() => {
 
-    // global variable with annotations for gonfaloni
-    const regionObjects = [];
-        // Load the JSON file with annotations
-        $.getJSON('assets/map-annotation-gonfaloni.json', function (data) {
-          // Create an array to hold the region objects
-          
+  // global object with annotations for gonfaloni so it can be accessed later
+  const regionObjects = [];
+  // how to log objects so you can see the content:
+  // console.log('regionObjects: ' + JSON.stringify(regionObjects)) 
 
-          // Loop through each region in the JSON file
-          for (const region of data.regions) {
-            const allPointsX = region.shape_attributes.all_points_x;
-            const allPointsY = region.shape_attributes.all_points_y;
-
-            // Combine the all_points_x and all_points_y arrays into a single string. This line was written by ChatGPT.
-            const pointsString = allPointsX.map((x, i) => `${x},${allPointsY[i]}`).join(' ');
-
-            // Create an object for the region with its name and formatted points string
-            const regionObj = {
-              name: region.region_attributes.name,
-              type: region.region_attributes.type,
-              avgWealth: region.region_attributes.avgwealth,
-              points: pointsString
-            };
-
-            // Add the region object to the array
-            regionObjects.push(regionObj);
-            
-          }
-
-          // Log the region objects to the console
-          //console.log('regionObjects: ' + JSON.stringify(regionObjects))
-          //return regionObjects
-        });
+  // get data for annotations from JSON here so it runs just once, not every time a checkbox is checked. The code to access the JSON file and reformat the pointsString was partly written by ChatGPT.
+  $.getJSON('assets/map-annotation-gonfaloni.json', function (data) {
+    // Create an array to hold the region objects
 
 
+    // Loop through each region in the JSON file
+    for (const region of data.regions) {
+      const allPointsX = region.shape_attributes.all_points_x;
+      const allPointsY = region.shape_attributes.all_points_y;
 
+      // Combine the all_points_x and all_points_y arrays into a single string. 
+      const pointsString = allPointsX.map((x, i) => `${x},${allPointsY[i]}`).join(' ');
+
+      // Create an object for the region with its name and formatted points string
+      const regionObj = {
+        name: region.region_attributes.name,
+        type: region.region_attributes.type,
+        avgWealth: region.region_attributes.avgwealth,
+        points: pointsString
+      };
+      console.log(regionObj.type)
+
+      // Add the region object to the array
+      regionObjects.push(regionObj);
+
+    }
+  });
+  // as soon as I use another file i get an error in the browser. To solve I set up a local testing server: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server
+  // to run this page in local server right-click HTML and select Launch in Browser (intro to extension in VSCode: https://marketplace.visualstudio.com/items?itemName=yuichinukiyama.vscode-preview-server&ssr=false#overview)
 
 
   // insert image in html ("prepend" puts the entity first in the container)
   //$('#oldmap-container').prepend($('<img>', { id: 'oldmap', src: 'images/basicmap.jpg' }))
 
 
-  // as soon as I use another file i get an error in the browser. To solve I set up a local testing server: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server
-  // to run this page in local server right-click HTML and select Launch in Browser (intro to extension in VSCode: https://marketplace.visualstudio.com/items?itemName=yuichinukiyama.vscode-preview-server&ssr=false#overview)
 
   // function to add annotation regions based on coordinates in map-annotation-gonfaloni.json
   function mapAnnotations(planNr, jsonFilePath) {
     $('input[id="' + planNr + '"]').click(function () {
       // if the checkbox is checked: 
       if ($(this).prop("checked") == true) {
-        // get data for annotations from JSON
 
-
-        //console.log('regionObjects: ' + JSON.stringify(regionObjects))
-        
         regionObjects.forEach(obj => {
           const nameString = obj.name;
           const pointsString = obj.points;
-          // The viewbox defines the SVG's size as the image size, preserveAspectRatio="none" means that the SVG overlay can be scaled and squished like the raster image instead of being cut off. Explanation for the use of SVGs: https://css-tricks.com/scale-svg/
-          $('#oldmap-container').append($('<div class="svg-container" id="generated"><svg viewBox="0 0 2178 2121" preserveAspectRatio="none"><polygon points="' + pointsString + '"style="fill:none;stroke:black;stroke-width:5" /><text x="200" y="100" fill="black">' + nameString + '</text></svg></div>'))
 
+          if (obj.type === "Quartieri") {
+            // Define the style of the line.
+            var styleString = "fill:none;stroke:black;stroke-width:4"
+          }
 
+          if (obj.type === "Gonfaloni") {
+            // Different line style for Gonfaloni: stroke-dasharray means dashed line
+            var styleString = "fill:none;stroke:black;stroke-dasharray:5,10;stroke-width:4"
+          }
+          // Insert SVG overlay as its own container since HTML can't handle SVG containers themselves, since they use different namespaces.
+          // The viewbox defines the SVG's size as the image size, preserveAspectRatio="none" means that the SVG overlay can be scaled and squished like the raster image instead of being cut off. 
+          // Explanation for the use of SVGs: https://css-tricks.com/scale-svg/
+          // Every city Quarter has its own overlay which means they properties can be changed individually. They have different name tags; if needed they could have eg. different colors or line patterns.
+          $('#oldmap-container').append($('<div class="svg-container" id="generated"><svg viewBox="0 0 2178 2121" preserveAspectRatio="none"><polygon points=" ' + pointsString + ' "style=' + styleString + ' /><text x="200" y="100" fill="black">' + nameString + '</text></svg></div>'))
         });
-        
 
-
-        // when the checkbox is unchecked again:  
+        // when the checkbox is unchecked again remove overlays
       } else if ($(this).prop("checked") == false) {
         $('.svg-container').remove();
       }
     });
   }
 
-  mapAnnotations("plan2", "assets/map-annotation-gonfaloni.json")
 
   function mapChoropleth(planNr, jsonFilePath) {
     $('input[id="' + planNr + '"]').click(function () {
@@ -142,13 +143,15 @@ $(document).ready(() => {
   }
 
   // call the function for each checkbox 
+  mapAnnotations("plan2", "assets/map-annotation-gonfaloni.json")
   mapIcons("plan6", "assets/examplejson.json");
   mapIcons("plan7", "assets/examplejson.json");
+  
 
   // place text: https://www.w3schools.com/howto/howto_css_image_text.asp
 
 
-  // Code for the Leaflat map of modern Florence. Reference: Agafonkin, Volodymyr. „Quick Start Guide - Leaflet - a JavaScript library for interactive maps“. Zugegriffen 22. April 2023. https://leafletjs.com/examples/quick-start/.
+  // Code for the Leaflat map of modern Florence. Edited version of: Agafonkin, Volodymyr. "Quick Start Guide - Leaflet - a JavaScript library for interactive maps". Accessed 22. April 2023. https://leafletjs.com/examples/quick-start/.
   var modernmap = L.map('modernmap').setView([43.7703, 11.2574], 14);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
