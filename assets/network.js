ab120fa3ae00fa7ec56820dc690f7ca9a3dd8ae8
Toni Massenberg <amassenberg@web.de>
2023-06-04 14:25:55 +0200

    basic working family network

--- assets/network.js
+++ assets/network.js
@@ -11,14 +11,15 @@ function akkordion(id) {
 }
 
 // The following function ForceGraph is based on code by Bruno Laranjeira from 2021 here: https://observablehq.com/@brunolaranjeira/d3-v6-force-directed-graph-with-directional-straight-arrow.
-// It was then adapted from Observable to vanilla JavaScript using ChatGPT.
-// I edited it to work with my data, filter types, add colors, etc.
+// It was then adapted from Observable to vanilla JavaScript and heavily edited using ChatGPT.
+// I edited it to work with my specific data, filter types, add colors, etc.
 
 // Code for the force-directed graph
 function ForceGraph() {
     const width = 800;
-    const height = 600;
+    const height = 1000;
 
+    // Add the SVG container for the network graph
     const svg = d3.select("#graph")
         .append("svg")
         .attr("width", width)
@@ -27,7 +28,7 @@ function ForceGraph() {
     // Define the arrowhead marker
     svg.append("defs").append("marker")
         .attr("id", "arrowhead")
-        .attr("viewBox", "-5 -5 10 10")
+        .attr("viewBox", "-5 -5 10 10") // One view box for every node
         .attr("refX", 8)
         .attr("refY", 0)
         .attr("markerWidth", 8)
@@ -37,100 +38,147 @@ function ForceGraph() {
         .attr("d", "M -5,-5 L 0,0 L -5,5")
         .attr("class", "arrowhead");
 
+    d3.csv("assets/data/painters-family.csv").then(function (connections) {
+        d3.csv("assets/data/painters.csv").then(function (labels) {
+            const nodes = Array.from(new Set(connections.flatMap(d => [d.source, d.target]))).map(id => {
+                const label = labels.find(p => p.id === id)?.label || "";
+                return { id, label };
+            });
 
-    d3.csv("assets/data.csv").then(function (data) {
-        const nodes = data.map((d) => ({ id: d.id, type: d.type }));
-        const links = data.map((d) => ({ source: d.source, target: d.target, type: d.type, lineType: d.lineType }));
+            const links = [];
 
-        const simulation = d3.forceSimulation(nodes)
-            .force("link", d3.forceLink(links).id((d) => d.id))
-            .force("charge", d3.forceManyBody().strength(-200))
-            .force("center", d3.forceCenter(width / 2, height / 2));
+            connections.forEach((d) => {
+                const sourceNode = nodes.find((node) => node.id === d.source);
+                const targetNode = nodes.find((node) => node.id === d.target);
 
-        const link = svg.selectAll(".link")
-            .data(links)
-            .enter()
-            .append("line")
-            .attr("class", "link")
-            .attr("stroke", (d) => getLineColor(d.lineType))
-            .attr("stroke-opacity", 0.6)
-            .attr("stroke-width", 1.5)
-            .attr("marker-end", "url(#arrowhead)");
+                // Create a link object for each connection
+                links.push({
+                    source: sourceNode,
+                    target: targetNode,
+                    type: d.type,
+                    lineType: d.lineType
+                });
+            });
 
-        const node = svg.selectAll(".node")
-            .data(nodes)
-            .enter()
-            .append("circle")
-            .attr("class", "node")
-            .attr("r", 10)
-            .attr("fill", (d) => getNodeColor(d.type))
-            .call(drag(simulation));
+            const linkForce = d3.forceLink(links)
+                .id((d) => d.id)
+                .distance(50); // Adjust the length of the links
 
-        node.append("title")
-            .text((d) => d.id);
 
-        simulation.on("tick", () => {
-            link.attr("x1", (d) => d.source.x)
-                .attr("y1", (d) => d.source.y)
-                .attr("x2", (d) => d.target.x)
-                .attr("y2", (d) => d.target.y);
+            const simulation = d3.forceSimulation(nodes)
+                .force("link", linkForce)
+                .force("charge", d3.forceManyBody().strength(-10)) // Force with which other nodes are repelled
+                .force("center", d3.forceCenter(width / 2, height / 2));
 
-            node.attr("cx", (d) => d.x)
-                .attr("cy", (d) => d.y);
+            const link = svg.selectAll(".link")
+                .data(links)
+                .enter()
+                .append("line")
+                .attr("class", "link")
+                .attr("stroke", (d) => getLineColor(d.lineType))
+                .attr("stroke-opacity", 0.6)
+                .attr("stroke-width", 1.5)
+                .attr("marker-end", "url(#arrowhead)");
+
+            const node = svg.selectAll(".node")
+                .data(nodes)
+                .enter()
+                .append("circle")
+                .attr("class", "node")
+                .attr("r", 5) // Node size
+                .attr("fill", (d) => getNodeColor(d.type))
+                .call(drag(simulation));
+
+            node.append("title") // Title will be displayed on hover
+                .text((d) => d.label);
+
+            // Define what happens when a node is dragged: x/y position changes
+            simulation.on("tick", () => {
+                link.attr("x1", (d) => d.source.x)
+                    .attr("y1", (d) => d.source.y)
+                    .attr("x2", (d) => d.target.x)
+                    .attr("y2", (d) => d.target.y);
+
+                node.attr("cx", (d) => d.x)
+                    .attr("cy", (d) => d.y);
+            });
+
+            function drag(simulation) {
+                function dragstarted(event) {
+                    if (!event.active) simulation.alphaTarget(0.3).restart();
+                    event.subject.fx = event.subject.x;
+                    event.subject.fy = event.subject.y;
+                }
+
+                function dragged(event) {
+                    event.subject.fx = event.x;
+                    event.subject.fy = event.y;
+                }
+
+                function dragended(event) {
+                    if (!event.active) simulation.alphaTarget(0);
+                    event.subject.fx = null;
+                    event.subject.fy = null;
+                }
+
+                return d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended);
+            }
+
+            function getNodeColor(type) {
+                // Define colors based on type / job
+                if (type === "tafelmaler") {
+                    return "red";
+                } else if (type === "moebelmaler") {
+                    return "blue";
+                } else if (type === "waffenmaler") {
+                    return "blue";
+                } else if (type === "moebelmaler") {
+                    return "blue";
+                } else if (type === "glasmaler") {
+                    return "blue";
+                } else if (type === "miniaturist") {
+                    return "blue";
+                } else if (type === "naibi") {
+                    return "blue";
+                } else if (type === "ceri") {
+                    return "blue";
+                } else if (type === "stoffmaler") {
+                    return "blue";
+                } else if (type === "gips") {
+                    return "blue";
+                } else if (type === "steinmetz") {
+                    return "blue";
+                } else if (type === "hobby") {
+                    return "blue";
+                } else if (type === "anstreicher") {
+                    return "blue";
+                } else {
+                    return "green";
+                }
+            }
+
+            function getLineColor(lineType) {
+                // Define colors based on lineType / relationship
+                if (lineType === "fatherOf") {
+                    return "red";
+                } else if (lineType === "brotherOf") {
+                    return "blue";
+
+                } else if (lineType === "uncleOf") {
+                    return "green";
+                } else {
+                    return "yellow";
+                }
+            }
+        }).catch(function (error) {
+            console.log("Error loading painters data:", error);
         });
-
-        function drag(simulation) {
-            function dragstarted(event) {
-                if (!event.active) simulation.alphaTarget(0.3).restart();
-                event.subject.fx = event.subject.x;
-                event.subject.fy = event.subject.y;
-            }
-
-            function dragged(event) {
-                event.subject.fx = event.x;
-                event.subject.fy = event.y;
-            }
-
-            function dragended(event) {
-                if (!event.active) simulation.alphaTarget(0);
-                event.subject.fx = null;
-                event.subject.fy = null;
-            }
-
-            return d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended);
-        }
-
-        function getNodeColor(type) {
-            // Define colors based on type / job
-            if (type === "TypeA") {
-                return "red";
-            } else if (type === "TypeB") {
-                return "blue";
-            } else {
-                return "green";
-            }
-        }
-
-        function getLineColor(lineType) {
-            // Define colors based on lineType / relationship
-            if (lineType === "TypeA") {
-                return "red";
-            } else if (lineType === "TypeB") {
-                return "blue";
-            } else {
-                return "green";
-            }
-        }
     }).catch(function (error) {
-        console.log("Error loading data:", error);
+        console.log("Error loading connections data:", error);
     });
 }
 
 ForceGraph();
-
-
-
-
